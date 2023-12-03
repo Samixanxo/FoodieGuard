95c4723a88c8758aec1e9b44315a76e53c063db8
Samuel Robayo <sroballo@lanzaderaproyectos.com>
2023-12-03 14:42:07 +0100

    Aplicación retrofit

--- app/src/main/java/com/example/foodie_guardv0/retrofitt/RetrofitClient.kt
+++ app/src/main/java/com/example/foodie_guardv0/retrofitt/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://localhost:8080/api/restaurant/1"
+    private const val BASE_URL = "http://localhost:8080/api/restaurant"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
