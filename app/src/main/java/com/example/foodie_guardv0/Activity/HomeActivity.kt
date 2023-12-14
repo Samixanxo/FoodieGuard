e44323ac8980615c57bb589baa1fd0f53a11bfeb
herjflorez <48648845+herjflorez@users.noreply.github.com>
2023-12-14 16:01:57 +0100

    menu

--- app/src/main/java/com/example/foodie_guardv0/Activity/HomeActivity.kt
+++ app/src/main/java/com/example/foodie_guardv0/Activity/HomeActivity.kt
@@ -1,28 +1,12 @@
 package com.example.foodie_guardv0.Activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.foodie_guard0.R
-import com.example.foodie_guardv0.dataclass.Restaurant
-import com.example.foodie_guardv0.dataclass.User
-import com.example.foodie_guardv0.restaurantAdapter.RestaurantAdapter
-import com.example.foodie_guardv0.retrofitt.ApiService
-import com.example.foodie_guardv0.retrofitt.RetrofitClient
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import kotlin.coroutines.suspendCoroutine
 
 class HomeActivity : AppCompatActivity() {
 
@@ -30,4 +14,38 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_view)
 
-}}
+        val a = search_Fragment()
+        val b = home_fragment()
+        val c = user()
+
+        val menu = findViewById<BottomNavigationView>(R.id.bottom_bar)
+
+        menu.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.search_item -> {
+                    set(a)
+                    true
+                }
+                R.id.home_item -> {
+                    set(b)
+                    true
+                }
+                R.id.user_item -> {
+                    set(c)
+                    true
+                }
+
+                else -> false
+            }
+
+    }
+
+
+}
+    private fun set(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.navHostFragment, fragment)
+            commit()
+        }
+    }
+}
--- app/src/main/java/com/example/foodie_guardv0/Activity/home_fragment.kt
+++ app/src/main/java/com/example/foodie_guardv0/Activity/home_fragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -27,7 +26,7 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-class home_fragment : Fragment(), SearchView.OnQueryTextListener {
+class home_fragment : Fragment(R.layout.fragment_home_fragment), SearchView.OnQueryTextListener {
 
     private val service = RetrofitClient.retrofit.create(ApiService::class.java)
 
