ed0b16f15e2ece33b7e0a50d3b1118621d8727a0
Miguelstucom <114105189+Miguelstucom@users.noreply.github.com>
2024-01-30 19:28:29 +0100

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.android.gms:play-services-location:21.1.0")
+    implementation ("com.google.android.gms:play-services-location:17.1.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- app/src/main/java/com/example/foodie_guardv0/Activity/MapFragment.kt
+++ app/src/main/java/com/example/foodie_guardv0/Activity/MapFragment.kt
@@ -1,51 +1,106 @@
 
-import android.content.Context
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.LocalContext
-import androidx.databinding.DataBindingUtil.setContentView
 import com.example.foodie_guard0.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-
 class MapFragment : Fragment(), OnMapReadyCallback {
 
-
     private lateinit var map: GoogleMap
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflar el diseño del fragmento
         val view = inflater.inflate(R.layout.fragment_map, container, false)
-        // Llama a createFragment para inicializar el mapa
+        createFragment()
         return view
     }
 
     private fun createFragment() {
-
-        val mapFragment: SupportMapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        createMarker()
+
+        if (checkLocationPermission()) {
+            map.isMyLocationEnabled = true
+            addMarkerAtUserLocation()
+            addMarkers()
+        } else {
+            requestLocationPermission()
+        }
     }
 
-    private fun createMarker() {
-        val coordinates = LatLng(41.403706, 2.173504)
-        val marker: MarkerOptions = MarkerOptions().position(coordinates).title("prueba")
-        map.addMarker(marker)
+    private fun checkLocationPermission(): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+    private fun addMarkers() {
+        val plazaCatalunyaLatLng = LatLng(41.3879, 2.1699)
+        val markerOptionsCatalunya = MarkerOptions()
+            .position(plazaCatalunyaLatLng)
+            .title("Plaza Catalunya")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+        map.addMarker(markerOptionsCatalunya)
+
+        val plazaUniversitatLatLng = LatLng(41.3867, 2.1688)
+        val markerOptionsUniversitat = MarkerOptions()
+            .position(plazaUniversitatLatLng)
+            .title("Plaza Universitat")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+        map.addMarker(markerOptionsUniversitat)
+    }
+
+    private fun addMarkerAtUserLocation() {
+        val fusedLocationClient: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        try {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val userLatLng = LatLng(location.latitude, location.longitude)
+                    val markerOptions = MarkerOptions()
+                        .position(userLatLng)
+                        .title("Tu ubicación actual")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                    map.addMarker(markerOptions)
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 15f))
+                }
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 }
+
 
