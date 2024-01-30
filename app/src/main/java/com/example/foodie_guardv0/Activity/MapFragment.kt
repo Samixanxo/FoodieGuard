8e4e910ccade24b2d8fbe21b03897b299fb316a1
Miguelstucom <114105189+Miguelstucom@users.noreply.github.com>
2024-01-30 17:17:32 +0100

    fix

--- app/src/main/java/com/example/foodie_guardv0/Activity/MapFragment.kt
+++ app/src/main/java/com/example/foodie_guardv0/Activity/MapFragment.kt
@@ -1,10 +1,12 @@
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.LocalContext
 import androidx.databinding.DataBindingUtil.setContentView
 import com.example.foodie_guard0.R
 import com.google.android.gms.maps.GoogleMap
@@ -16,6 +18,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
+
     private lateinit var map: GoogleMap
 
     override fun onCreateView(
@@ -24,14 +27,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         // Inflar el diseño del fragmento
         val view = inflater.inflate(R.layout.fragment_map, container, false)
-
         // Llama a createFragment para inicializar el mapa
-        createFragment()
-
         return view
     }
 
     private fun createFragment() {
+
         val mapFragment: SupportMapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
