38749a5471abd7aa79a4cd9a27714981c969a477
Samuel Robayo <samirobayo04@gmail.com>
2024-02-02 15:21:49 +0100

    Un texto

--- app/src/main/java/com/example/foodie_guardv0/retrofitt/RetrofitClient.kt
+++ app/src/main/java/com/example/foodie_guardv0/retrofitt/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.118.1.106:8080/api/"
+    private const val BASE_URL = "http://10.118.3.77:8080/api/"
 
 
     val retrofit: Retrofit by lazy {
