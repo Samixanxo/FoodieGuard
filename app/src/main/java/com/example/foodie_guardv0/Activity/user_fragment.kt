97902ad0009f30f4934946520cde651dd1e48e7a
Samuel Robayo <samirobayo04@gmail.com>
2024-02-02 16:50:09 +0100

    Change password popUp

--- app/src/main/java/com/example/foodie_guardv0/Activity/user_fragment.kt
+++ app/src/main/java/com/example/foodie_guardv0/Activity/user_fragment.kt
@@ -3,14 +3,20 @@ package com.example.foodie_guardv0.Activity
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.PopupWindow
 import android.widget.TextView
+import android.graphics.Color
+import android.view.animation.AnimationUtils
 
 import com.example.foodie_guard0.R
 import com.example.foodie_guardv0.deprecated.LoginActivity
@@ -58,8 +64,51 @@ class user_fragment : Fragment() {
             activity?.finish()
         }
 
+        val editButton = view.findViewById<Button>(R.id.EditButton)
+        editButton.setOnClickListener {
+            showPopupWindow(inflater, view)
+
+        }
+
 
         return view
+    }
+
+    private fun showPopupWindow(inflater: LayoutInflater, parentView: View) {
+
+        val popupView = inflater.inflate(R.layout.edit_menu, null)
+        val animation = AnimationUtils.loadAnimation(context, R.anim.pop_up_menu)
+        popupView.startAnimation(animation)
+
+
+        parentView.findViewById<View>(R.id.background_overlay).visibility = View.VISIBLE
+        parentView.findViewById<View>(R.id.UserCard).visibility = View.INVISIBLE
+        val cancelButton = popupView.findViewById<Button>(R.id.canecelChangeButton)
+
+
+
+        val popupWindow = PopupWindow(
+            popupView,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            true
+        )
+        cancelButton.setOnClickListener {
+            parentView.findViewById<View>(R.id.background_overlay).visibility = View.INVISIBLE
+            parentView.findViewById<View>(R.id.UserCard).visibility = View.VISIBLE
+            popupWindow.dismiss()
+        }
+        popupWindow.isOutsideTouchable = false
+        popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        popupWindow.showAtLocation(
+            parentView,
+            Gravity.TOP,
+            0,
+            0
+        )
+
+
     }
 }
 
