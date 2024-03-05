f1308ce9352a51c9515e57321e1fb532ba5a520a
Miguelstucom <114105189+Miguelstucom@users.noreply.github.com>
2024-03-05 16:52:14 +0100

    a

--- app/src/main/java/com/example/foodie_guardv0/Activity/search_Fragment.kt
+++ app/src/main/java/com/example/foodie_guardv0/Activity/search_Fragment.kt
@@ -169,7 +169,7 @@ class search_Fragment : Fragment() {
     }
 
     private fun updateButtonState(button: ImageButton?, isSelected: Boolean) {
-        button?.setBackgroundResource(if (isSelected) R.color.grey else android.R.color.transparent)
+        button?.setBackgroundResource(if (isSelected) R.color.black else android.R.color.transparent)
     }
 
     private fun setupImageButtons(view: View) {
