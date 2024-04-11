88aa6e8d2b568831293e916a58dd4f99fca7b79d
Miguelstucom <114105189+Miguelstucom@users.noreply.github.com>
2024-04-11 16:54:18 +0200

    fix

--- app/src/main/java/com/example/foodie_guardv0/Activity/RegisterActivity.kt
+++ app/src/main/java/com/example/foodie_guardv0/Activity/RegisterActivity.kt
@@ -10,7 +10,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.example.foodie_guardv0.dataclass.ActualUser
